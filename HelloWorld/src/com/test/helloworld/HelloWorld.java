406b88ac7b9221d4c4ecd837938fa7cd83dcaed6
박초롱 <dpdltl78@naver.com>
2017-01-13 14:12:15 +0900

    초기 버전

--- HelloWorld/src/com/test/helloworld/HelloWorld.java
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -3,6 +3,8 @@ package com.test.helloworld;
 public class HelloWorld {
 	public static void main(String args[]){
 		System.out.println("hello wolrd");
+		System.out.println("æ»≥Á«œººø‰!");
+		
 	}
 	
 }
---
+++ VariableExam/src/com/test/Variable_exam/VariableExam.java
@@ -0,0 +1,9 @@
+package com.test.Variable_exam;
+
+public class VariableExam {
+	public static void main(String args[]){
+		int Value=10;
+		int result = Value +10;
+		System.out.println(result);
+	}
+}
